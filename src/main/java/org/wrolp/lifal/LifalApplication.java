cc0a9b09ec9e0924710717a6aae2c74dfcb29f24
vant <hgtk11@163.com>
2018-05-10 21:44:45 +0800

    init
    Signed-off-by: vant <hgtk11@163.com>

---
+++ src/main/java/org/wrolp/lifal/LifalApplication.java
@@ -0,0 +1,13 @@
+package org.wrolp.lifal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wrolp/lifal/LifalApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wrolp.lifal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LifalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
